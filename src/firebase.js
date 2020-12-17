32b7f39244c9cf23fb667ee4bfc288b7767a40d9
Cristopher Zaiz <e.zaizortega@gmail.com>
2020-12-16 20:32:48 -0400

    Configuration firebase

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD0YZKFoqcou1-CZRmnF45aAsBh46kvnJw",
+    authDomain: "auth-2020-b9385.firebaseapp.com",
+    projectId: "auth-2020-b9385",
+    storageBucket: "auth-2020-b9385.appspot.com",
+    messagingSenderId: "257294506516",
+    appId: "1:257294506516:web:eaf4eb34a1429e0f6ca501"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
+
+
