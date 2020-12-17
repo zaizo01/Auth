7d6c9e5d7f0dab3fad5f9072a7019dae832729b9
Cristopher Zaiz <e.zaizortega@gmail.com>
2020-12-16 20:50:09 -0400

    Register View and form created

---
+++ src/views/Register.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h1>Register</h1>
+        <form>
+            <input 
+                type="text"
+                placeholder="Email"
+                v-model="email"
+            >
+            <input 
+                type="password"
+                placeholder="Password"
+                v-model="firstPass"
+            >
+            <input 
+                type="password"
+                placeholder="Password"
+                v-model="secondPass"
+            >
+            <button type="submit">Sign in</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Register',
+    data() {
+        return {
+            email: '',
+            firstPass: '',
+            secondPass: ''
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
